4673f183a85f5484a42f88ed65fa97d5146bf6ad
Danush Ramanan <dan.ramanan@gmail.com>
2025-02-23 13:30:35 -0700

    made base worldgen code TODO: generate voxlel data, render meshes

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include "raylib.h"
 #include <cmath>
 #include <memory>
+#include <vector>
 
 #define MOUSE_SENSITIVITY 0.003f
 #define SPEED 5.0f
+#define BLOCK_SIZE 16
 
 class FreeCam
 {
@@ -21,22 +23,22 @@ public:
         camera.projection = CAMERA_PERSPECTIVE;
     }
 
-    void Update()
+    void update()
     {
         // calculate directions
-        Vector2 mouseDelta = GetMouseDelta();
-        yaw += mouseDelta.x * MOUSE_SENSITIVITY;
-        pitch -= mouseDelta.y * MOUSE_SENSITIVITY;
+        auto [x, y] = GetMouseDelta();
+        yaw += x * MOUSE_SENSITIVITY;
+        pitch -= y * MOUSE_SENSITIVITY;
 
         if (pitch > 1.5f) pitch = 1.5f;
         if (pitch < -1.5f) pitch = -1.5f;
 
-        Vector3 forward = {
+        const Vector3 forward = {
             cosf(yaw) * cosf(pitch),
             sinf(pitch),
             sinf(yaw) * cosf(pitch)
         };
-        Vector3 right = {
+        const Vector3 right = {
             cosf(yaw - PI / 2.0f), // Rotate 90° counterclockwise
             0.0f,
             sinf(yaw - PI / 2.0f)
@@ -78,6 +80,91 @@ public:
     }
 };
 
+enum BlockTypes
+{
+    Air,
+    Grass
+};
+struct Block
+{
+    int x, y, z;
+    int blockType;
+    bool isSolid;
+
+    Block(const int x, const int y, const int z, const int blockType, const bool isSolid)
+    {
+        this->x = x;
+        this->y = y;
+        this->z = z;
+        this->blockType = blockType;
+        this->isSolid = isSolid;
+    }
+};
+
+class World
+{
+    std::vector<std::vector<std::vector<Block>>> blocks;
+
+    float vertices[];
+    unsigned short indices[];
+
+    static int generateHeight(const int x, const int y) // generate terrain based on perlin noise
+    {
+        return 0;
+    }
+
+    void generateWorld(const int width, const int depth, const int height)
+    {
+        // generate heightmap
+        std::vector heightMap(width, std::vector<int>(depth));
+        for (int i = 0; i < width; i++)
+        {
+            for (int j = 0; j < depth; j++)
+            {
+                heightMap[i][j] = generateHeight(i, j);
+            }
+        }
+
+        // generate an internal representation of the world based on the heightMap
+        blocks = std::vector(64, std::vector(64, std::vector<Block>(64)));
+        for (int i = 0; i < 64; i++) // iterate over depth (z-axis)
+        {
+            for (int j = 0; j < 64; j++) // iterate over width (x-axis)
+            {
+                const int columnHeight = heightMap[i][j];
+
+                // generate blocks up to the height determined by the heightmap
+                for (int k = 0; k < height; k++) // iterate over height (y-axis)
+                {
+                    if (k <= columnHeight)
+                    {
+                        blocks[i][k][j] = Block(i, k, i, Grass, true);
+                        continue;
+                    }
+                    blocks[i][k][j] = Block(i, k, i, Air, false);
+                }
+            }
+        }
+
+        // generate voxel data
+    }
+
+public:
+    World(const int world_width, const int world_depth, const int world_height)
+    {
+        generateWorld(world_width, world_depth, world_height);
+    }
+    ~World() = delete;
+
+
+
+    void render()
+    {
+
+    }
+};
+
+
 int main() {
     InitWindow(800, 600, "Voxel Terrain");
     DisableCursor(); // Hide and lock cursor to the window
@@ -87,7 +174,7 @@ int main() {
     SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
-        freeCam->Update();
+        freeCam->update();
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
